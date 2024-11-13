3ca6e51e93732f4cb9b0dba23889270c46c80cd4
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-11-13 09:41:42 +0700

    themes

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/shared/shared.dart';
 import 'package:flutter/material.dart';
 
 part 'ui/home_page.dart';
--- lib/pages/ui/home_page.dart
+++ lib/pages/ui/home_page.dart
@@ -6,6 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: primaryColor,
       body: Center(
         child: Image.asset('assets/nathan.JPG'),
       ),
