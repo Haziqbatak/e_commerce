50b3acce4a743952c9ea1e571541f0bdb97fbdd5
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-11-14 13:07:24 +0700

    banyaaak

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -5,4 +5,8 @@ part 'ui/home_page.dart';
 part 'ui/sign_in_page.dart';
 part 'ui/splash_screen_page.dart';
 part 'ui/sign_up_page.dart';
+part 'ui/main_page.dart';
+part 'ui/wish_list_page.dart';
+part 'ui/profile_page.dart';
+part 'ui/chat_page.dart';
 
---
+++ lib/pages/ui/chat_page.dart
@@ -0,0 +1,11 @@
+part of '../pages.dart';
+
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
