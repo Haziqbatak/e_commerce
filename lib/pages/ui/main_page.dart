c0b07046d260f54a86c9d48b8ee8df8651e24a24
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-11-14 20:41:46 +0700

    float

--- lib/pages/ui/main_page.dart
+++ lib/pages/ui/main_page.dart
@@ -3,8 +3,23 @@ part of '../pages.dart';
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
+  Widget floatButton() {
+    return FloatingActionButton(
+      onPressed: () {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: backgroundColor1,
+      floatingActionButton: floatButton(),
+      body: Center(
+        child: Text(
+          'Main Page',
+          style: primaryTextStyle,
+        ),
+      ),
+    );
   }
 }
