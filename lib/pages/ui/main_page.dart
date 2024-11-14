ea34bdcf2aa63614d3797daa9844866b0b2b9778
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-11-14 21:39:09 +0700

    makin numpuk errorrr

--- lib/pages/ui/main_page.dart
+++ lib/pages/ui/main_page.dart
@@ -1,11 +1,93 @@
 part of '../pages.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
+
+  Widget body() {
+    switch (currentIndex) {
+      case 0:
+        return HomePage();
+      case 1:
+        return ChatPage();
+      case 2:
+        return WishListPage();
+      case 3:
+        return ProfilePage();
+      default:
+        return HomePage();
+    }
+  }
+
+  Widget customButtonNav() {
+    return ClipRRect(
+      borderRadius: BorderRadius.vertical(
+        top: Radius.circular(30),
+      ),
+      clipBehavior: Clip.antiAlias,
+      child: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 12,
+        child: BottomNavigationBar(
+          backgroundColor: backgroundColor4,
+          type: BottomNavigationBarType.fixed,
+          onTap: (value) {
+            setState(() {
+              currentIndex = value;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/icon_home.png',
+                width: 24,
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/icon_chat.png',
+                width: 24,
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/icon_wishlist.png',
+                width: 24,
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/icon_profile.png',
+                width: 24,
+              ),
+              label: '',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   Widget floatButton() {
     return FloatingActionButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(50),
+      ),
+      backgroundColor: primaryColor,
       onPressed: () {},
+      child: Image.asset(
+        'assets/icon_cart.png',
+        width: 24,
+      ),
     );
   }
 
@@ -14,6 +96,8 @@ class MainPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: backgroundColor1,
       floatingActionButton: floatButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: customButtonNav(),
       body: Center(
         child: Text(
           'Main Page',
