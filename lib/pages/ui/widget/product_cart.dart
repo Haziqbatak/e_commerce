fcbf6bd58e32eaa9059386b3918e2170fbdeede4
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-11-26 09:33:38 +0700

    ***

--- lib/pages/ui/widget/product_cart.dart
+++ lib/pages/ui/widget/product_cart.dart
@@ -14,34 +14,53 @@ class ProductCart extends StatelessWidget {
       decoration: BoxDecoration(
         color: whiteColor,
         borderRadius: BorderRadius.vertical(
-          top: Radius.circular(25),
+          top: Radius.circular(
+            25,
+          ),
         ),
         image: DecorationImage(
-          image: AssetImage('assets/image_shoes.png'),
+          image: AssetImage(
+            'assets/image_shoes.png',
+          ),
           fit: BoxFit.cover,
         ),
       ),
       child: Container(
-        margin: EdgeInsets.only(top: 10, left: 15, right: 15, bottom: 15),
+        margin: EdgeInsets.only(
+          top: 10,
+          left: 15,
+          right: 15,
+          bottom: 15,
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               'Shoes',
-              style:
-                  primaryTextStyle.copyWith(fontSize: 14, fontWeight: semiBold),
+              style: primaryTextStyle.copyWith(
+                fontSize: 14,
+                fontWeight: semiBold,
+              ),
             ),
             Text(
               'Sepatu Gunung Arei V2',
               style: primaryTextStyle.copyWith(
-                  fontSize: 14, fontWeight: semiBold, color: blackColor),
+                fontSize: 14,
+                fontWeight: semiBold,
+                color: blackColor,
+              ),
               overflow: TextOverflow.ellipsis,
             ),
             Text(
               NumberFormat.currency(
-                      symbol: 'IDR', locale: 'id_ID', decimalDigits: 0)
-                  .format(100000),
-              style: priceTextStyle.copyWith(fontWeight: medium, fontSize: 14),
+                symbol: 'IDR',
+                locale: 'id_ID',
+                decimalDigits: 0,
+              ).format(100000),
+              style: priceTextStyle.copyWith(
+                fontWeight: medium,
+                fontSize: 14,
+              ),
             ),
           ],
         ),
