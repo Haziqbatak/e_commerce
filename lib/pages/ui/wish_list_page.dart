ffd9d16d888719c78f51c5a0becd2cd3ba6987c5
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-12-04 20:38:21 +0700

    KHAKHAKHAKHAKHAKHAKHAKHA

--- lib/pages/ui/wish_list_page.dart
+++ lib/pages/ui/wish_list_page.dart
@@ -3,6 +3,19 @@ part of '../pages.dart';
 class WishListPage extends StatelessWidget {
   const WishListPage({super.key});
 
+  PreferredSizeWidget header() {
+    return AppBar(
+      backgroundColor: backgroundColor1,
+      centerTitle: true,
+      title: Text(
+        'Favorite List',
+        style: primaryTextStyle.copyWith(fontSize: 18, fontWeight: medium),
+      ),
+      elevation: 0,
+      automaticallyImplyLeading: false,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget emptyWishList() {
@@ -11,39 +24,44 @@ class WishListPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset(
-              'assets/image_wishList.png',
+              'assets/image_wishlist.png',
               width: 100,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'You don\'t have dream stuff ?',
+              style:
+                  primaryTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+            ),
+            Text(
+              'Lets find your favorite',
+              style: primaryTextStyle,
+            ),
+            SizedBox(height: 20),
+            OutlinedButton(
+              onPressed: () {},
+              child: Text(
+                'Explore Store',
+              ),
+              style: OutlinedButton.styleFrom(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 24,
+                  vertical: 10,
+                ),
+                side: BorderSide(
+                  color: primaryColor,
+                ),
+              ),
             ),
           ],
         ),
       );
     }
 
-    PreferredSizeWidget header() {
-      return AppBar(
-        backgroundColor: backgroundColor1,
-        centerTitle: true,
-        title: Text(
-          'Wishlist page',
-          style: primaryTextStyle.copyWith(
-            fontSize: 18,
-            fontWeight: medium,
-          ),
-        ),
-        elevation: 0,
-        automaticallyImplyLeading: false,
-      );
-    }
-
-    return Scaffold(
-      backgroundColor: backgroundColor1,
-      appBar: header(),
-      body: Center(
-        child: Text(
-          'WishList Page',
-          style: primaryTextStyle,
-        ),
-      ),
+    return Column(
+      children: [header(), emptyWishList()],
     );
   }
 }
