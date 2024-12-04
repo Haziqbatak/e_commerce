5b5e88df4676bab4c4ea2db377739c88ee1b3f31
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-12-04 20:20:14 +0700

    hahahaha

--- lib/pages/ui/wish_list_page.dart
+++ lib/pages/ui/wish_list_page.dart
@@ -5,8 +5,19 @@ class WishListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    PreferredSizeWidget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        title: Text(
+          'Wishlist page',
+          style: primaryTextStyle,
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: backgroundColor1,
+      appBar: header(),
       body: Center(
         child: Text(
           'WishList Page',
