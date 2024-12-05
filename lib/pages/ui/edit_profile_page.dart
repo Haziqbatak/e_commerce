391c791d4457a8fca8020ad6395b203c42c9e5b6
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-12-05 14:48:54 +0700

    ...

--- lib/pages/ui/edit_profile_page.dart
+++ lib/pages/ui/edit_profile_page.dart
@@ -5,6 +5,51 @@ class EditProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Widget content() {
+      return Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 64,
+              backgroundImage: AssetImage(
+                'assets/image_profile.png',
+              ),
+            ),
+            Text(
+              'Haziq',
+              style: primaryTextStyle,
+            )
+          ],
+        ),
+      );
+    }
+
+    PreferredSizeWidget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          color: Colors.white,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text(
+          'Edit Profile',
+          style: primaryTextStyle.copyWith(
+            fontWeight: semiBold,
+            fontSize: 20,
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor2,
+      appBar: header(),
+      body: content(),
+    );
   }
 }
