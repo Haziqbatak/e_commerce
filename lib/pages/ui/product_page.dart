9e628d8f5c09bb9e9df83fd5c777b3b2d53e034e
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-12-07 09:58:58 +0700

    n

--- lib/pages/ui/product_page.dart
+++ lib/pages/ui/product_page.dart
@@ -1,15 +1,123 @@
 part of '../pages.dart';
 
-class ProductPage extends StatelessWidget {
+class ProductPage extends StatefulWidget {
   const ProductPage({super.key});
 
   @override
+  State<ProductPage> createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  int currentIndex = 0;
+
+  List imagesProduct = [
+    'assets/image_shoes1.png',
+    'assets/image_shoes2.png',
+    'assets/image_shoes3.png',
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'Product Page'
+    Widget indicatorBar(int index) {
+      return Container(
+        height: 10,
+        width: currentIndex == index ? 30 : 10,
+        margin: EdgeInsets.symmetric(
+          horizontal: 2,
         ),
+        decoration: BoxDecoration(
+          color: currentIndex == index ? priceColor : Colors.white,
+          borderRadius: currentIndex == index 
+          ? BorderRadius.circular(10)
+          : BorderRadius.circular(5),
+          border: Border.all(
+            color: priceColor
+          )
+        ),
+      );
+    }
+
+    Widget header() {
+      return Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(
+              top: 20,
+              right: 20,
+              left: 20,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.shopping_bag_outlined,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.shopping_bag,
+                    size: 30,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          CarouselSlider(
+            items: imagesProduct
+                .map(
+                  (elemen) => Image.asset(
+                    elemen,
+                    width: MediaQuery.of(context).size.width,
+                    fit: BoxFit.cover,
+                  ),
+                )
+                .toList(),
+            options: CarouselOptions(
+              height: 210,
+              enableInfiniteScroll: false,
+              viewportFraction: 1,
+              autoPlay: true,
+              enlargeCenterPage: true,
+              autoPlayInterval: Duration(seconds: 3),
+              
+              onPageChanged: (index, reason) {
+                setState(() {
+                  currentIndex = index;
+                });
+              },
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  for (var i = 0; i < imagesProduct.length; i++)
+                  indicatorBar(i),
+                ],
+              )
+            ],
+          )
+        ],
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor6,
+      body: ListView(
+        children: [
+          header(),
+        ],
       ),
     );
   }
